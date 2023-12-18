13a69e077c58440f9501f7bcb4c267cb138149f3
ricardo-quintela <ricardo.dev185@gmail.com>
2023-12-18 22:45:54 +0000

    fix: removed long description from setup.py

--- src/setup.py
+++ src/setup.py
@@ -2,11 +2,6 @@ from setuptools import setup, find_packages
 import codecs
 import os
 
-here = os.path.abspath(os.path.dirname(__file__))
-
-with codecs.open(os.path.join(here, "../README.md"), encoding="utf-8") as fh:
-    long_description = "\n" + fh.read()
-
 VERSION = '0.1'
 DESCRIPTION = 'Tools to make a compiler in python'
 LONG_DESCRIPTION = 'A package that contains a lexer and a parser that creates an AST.'
@@ -18,7 +13,7 @@ setup(
     author="ricardoquinteladev",
     description=DESCRIPTION,
     long_description_content_type="text/markdown",
-    long_description=long_description,
+    long_description=LONG_DESCRIPTION,
     packages=find_packages(),
     keywords=['python', 'compiler', 'lexer', 'parser', 'lalr', 'ast'],
     classifiers=[
